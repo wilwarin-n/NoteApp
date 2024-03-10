4542b59f928b87a8fdaaef18d3eb585971ce219d
Rumeysa <michiru.aware.lrn@gmail.com>
2024-03-10 19:07:34 +0300

    Added userdefaults and casting
    viewDidLoad() and saveClicked()

--- NoteApp/ViewController.swift
+++ NoteApp/ViewController.swift
@@ -19,10 +19,24 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let savedNote = UserDefaults.standard.object(forKey: "note")
+        let savedTime = UserDefaults.standard.object(forKey: "time")
+        
+        //as -> casting
+        if let getNote = savedNote as? String {
+            notelabel.text = "To-Do: \(getNote)"
+        }
+        
+        if let getTime = savedTime as? String {
+            timelabel.text = "The Time for To-Do: \(getTime)"
+        }
     }
 
     @IBAction func saveClicked(_ sender: Any) {
+        
+        UserDefaults.standard.set(noteTextField.text!, forKey: "note")
+        UserDefaults.standard.set(timeTextfield.text!, forKey: "time")
         
         notelabel.text = "To-Do: \(noteTextField.text!)"
         timelabel.text = "The Time for To-do: \(timeTextfield.text!)"
